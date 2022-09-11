542168e47d0bb17da125fc40f9d06fd99fd765f8
Vitaliy Rodyna <ukrdao@gmail.com>
2022-09-11 12:55:57 +0300

    this.image = new Image();         this.image.src = imageSrc;

--- js/classes.js
+++ js/classes.js
@@ -10,7 +10,8 @@ class Sprite{
         this.position = position;
         this.width = 50;
         this.height = 150;
-
+        this.image = new Image();
+        this.image.src = imageSrc;
 
 
     }
