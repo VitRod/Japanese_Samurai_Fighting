3d96a68bb4c2b5416b27702ca7eda6e20fb704b3
Vitaliy Rodyna <ukrdao@gmail.com>
2022-09-11 10:58:49 +0300

    constructor({ position,         imageSrc,         scale = 1,         framesMax = 1,         offset = { x: 0, y: 0 }     })

--- js/classes.js
+++ js/classes.js
@@ -1,6 +1,24 @@
 //Sprite(Animation) Class
 class Sprite{
     //Generate Animation
+    constructor({ position,
+        imageSrc,
+        scale = 1,
+        framesMax = 1,
+        offset = { x: 0, y: 0 }
+    }){
+
+
+
+        
+    }
+
+
+
+
+
+
+
 
 
 }
