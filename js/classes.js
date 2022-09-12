c873646190e9c4328311cbb6e919965d9fe6ea7b
Vitaliy Rodyna <ukrdao@gmail.com>
2022-09-12 07:47:58 +0300

    if (this.framesCurrent < this.framesMax - 1) {

--- js/classes.js
+++ js/classes.js
@@ -42,6 +42,7 @@ class Sprite{
     animateFrames() {
         this.framesElapsed++;
         if (this.framesElapsed % this.framesHold === 0) {
+            if (this.framesCurrent < this.framesMax - 1) {
 
     }
 
