5a6161716b95734aeae0a4f8e5fb90760656603d
Vitaliy Rodyna <ukrdao@gmail.com>
2022-09-15 07:36:27 +0300

    //Constant Values const gravity = 0.7; const fwdSpeed = 5; const bwdSpeed = -5; const jumpStregth = -20;

--- index.js
+++ index.js
@@ -4,4 +4,10 @@ const context = canvas.getContext('2d');
 
 //Canvas Area
 canvas.width = 1024;
-canvas.height = 576;   
+canvas.height = 576;  
+
+//Constant Values
+const gravity = 0.7;
+const fwdSpeed = 5;
+const bwdSpeed = -5;
+const jumpStregth = -20;
